6022b31123a3c498d0dd83c74d178a1a92190fb9
Gonzalo <46361784+GonLes@users.noreply.github.com>
2023-09-10 17:04:22 -0300

    Creacion de views
    se crearon las views para los modelos

--- Proyectos/models.py
+++ Proyectos/models.py
@@ -9,3 +9,14 @@ class ProjectManager(models.Model):
     nombre = models.CharField(max_length=50)
     apellido = models.CharField(max_length=50)
     email=models.CharField(max_length=50)
+
+opciones = [
+    ('opcion1', 'Sin Iniciar'),
+    ('opcion2', 'Iniciada'),
+    ('opcion3', 'Terminada'),
+]
+class Tareas(models.Model):
+    nombreTarea = models.CharField( max_length=50, blank=False )
+    fechaInicio=models.DateField()
+    fechaTermino=models.DateField()
+    estado=models.CharField(max_length=20, choices=opciones)
--- Proyectos/views.py
+++ Proyectos/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
-
+from django.http import HttpResponse
+from .models import Proyecto
+from .models import ProjectManager
+from .models import Tareas
 # Create your views here.
+def proyecto(req, nombre, codigo):
+    proyecto=Proyecto(nombre=nombre,codigo=codigo)
+    proyecto.save()
+    return HttpResponse (f"""
+    <p> Proyecto:{proyecto.nombre} con codigo {proyecto.codigo} ha sido creado)""")
+
+def projectManager(req, nombre, apellido,email):
+    projectManager=ProjectManager(nombre=nombre,apellido=apellido,email=email)
+    projectManager.save()
+    return HttpResponse (f"""
+    <p> Jefe de proyecto:{projectManager.nombre} {projectManager.apellido} ha sido creado </p>""")
+
+def tarea(req,nombreTarea,fechaInicio,fechaTermino,estado):
+    tarea=Tareas(nombreTarea=nombreTarea,fechaInicio=fechaInicio,fechaTermino=fechaTermino,estado=estado)
+    tarea.save()
+    return HttpResponse (f"""
+    <p> Tarea:{tarea.nombreTarea} ha sido creada)""")
+
+
+
+
+
+
