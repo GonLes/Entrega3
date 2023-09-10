1f3e5b495aeffe6277254bd8822d1a241c359739
Gonzalo <46361784+GonLes@users.noreply.github.com>
2023-09-10 16:22:06 -0300

    creacion de modelo
    se creo modelo
    se agrego la app Proyectos a settings.py
    se migro la bbdd

--- Proyectos/models.py
+++ Proyectos/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+class Proyecto(models.Model):
+    nombre = models.CharField( max_length=40)
+    codigo = models.IntegerField()
+
+class ProjectManager(models.Model):
+    nombre = models.CharField(max_length=50)
+    apellido = models.CharField(max_length=50)
+    email=models.CharField(max_length=50)
