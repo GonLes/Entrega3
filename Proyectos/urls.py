df9fd87b62b10972187eccdf223e110ceec50eb5
Gonzalo <46361784+GonLes@users.noreply.github.com>
2023-09-11 17:02:55 -0300

    creada las herencias padre a todos los html
    creada las herencias padre a todos los html

--- Proyectos/urls.py
+++ Proyectos/urls.py
@@ -3,8 +3,8 @@ from Proyectos import views
 from .views import *
 
 urlpatterns = [
-    path('', inicio),
-    path('proyectos/', proyectos),
-    path('projectManagers/', projectManagers),
-    path('tareas/', tareas),
+    path('', inicio, name= "Inicio"),
+    path('Proyectos/', proyectos, name="Proyectos"),
+    path('ProjectManagers/', projectManagers, name="ProjectManagers"),
+    path('Tareas/', tareas, name="Tareas"),
 ]
